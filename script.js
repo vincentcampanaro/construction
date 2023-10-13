e6cdd0d6ca501f931cb029d78abfe048896a11a1
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-10-13 15:05:57 -0400

    script.js

--- script.js
+++ script.js
@@ -2,30 +2,25 @@
 const map = L.map('map').setView([40.7128, -74.0060], 13);
 
 // Add a modern Mapbox base map
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmluY2VudGNhbXBhbmFybyIsImEiOiJjbG5vMDFnaW0wOWZrMmxxZGRhZGpxc2poIn0.CAmYZ_D7znuGjGsEMpVtsA', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=your-access-token', {
     attribution: '&copy; <a href="https://www.mapbox.com/">Mapbox</a> contributors',
     id: 'mapbox/streets-v11',  // Choose a Mapbox style
 }).addTo(map);
 
 // Function to add marker to map using project data
 function addMarker(project) {
-    // Check if latitude and longitude are defined
-    if (project.latitude && project.longitude) {
-        const latLng = L.latLng(project.latitude, project.longitude);
-        L.circleMarker(latLng, { radius: 5, color: 'blue', fillColor: 'blue', fillOpacity: 1 })
-            .bindPopup(`
-                <b>${project.name}</b><br>
-                ${project.building_address}<br>
-                ${project.city}, ${project.zip_code}<br>
-                Project Description: ${project.projdesc}<br>
-                Construction Award: ${project.award}<br>
-                Project Type: ${project.consttype}<br>
-                <button onclick="searchGoogle('${project.name}', '${project.building_address}')">Search with Google</button>
-            `)
-            .addTo(map);
-    } else {
-        console.warn('Missing latitude or longitude:', project);
-    }
+    const latLng = L.latLng(project.latitude, project.longitude);
+    L.circleMarker(latLng, { radius: 5, color: 'blue', fillColor: 'blue', fillOpacity: 1 })
+        .bindPopup(`
+            <b>${project.name}</b><br>
+            ${project.building_address}<br>
+            ${project.city}, ${project.zip_code}<br>
+            Project Description: ${project.projdesc}<br>
+            Construction Award: ${project.award}<br>
+            Project Type: ${project.consttype}<br>
+            <button onclick="searchGoogle('${project.name}', '${project.building_address}')">Search with Google</button>
+        `)
+        .addTo(map);
 }
 
 // Function to search Google
@@ -39,10 +34,13 @@ let data;
 fetch('https://data.cityofnewyork.us/resource/8586-3zfm.json')
     .then(response => response.json())
     .then(fetchedData => {
-        console.log(fetchedData);  // Log the fetched data
         data = fetchedData;
         data.forEach(project => {
-            addMarker(project);
+            if (project.latitude && project.longitude) {  // Check if latitude and longitude are defined
+                addMarker(project);
+            } else {
+                console.warn('Missing latitude or longitude:', project);
+            }
         });
     })
     .catch(error => {
