a3e3cfca66a27e294c19c6b7e02c6c2949fec8f3
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-10-12 22:32:30 -0400

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Set a custom icon if the default icon is not loading
+const myIcon = L.icon({
+    iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
+    iconSize: [38, 95],
+});
+
 // Function to geocode address and add marker to map
 function geocodeAndAddMarker(address) {
     const apiKey = 'dabe268c136f4d0aa3b1f7c265dc0516';
@@ -14,8 +20,9 @@ function geocodeAndAddMarker(address) {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             const latLng = L.latLng(data.results[0].geometry.lat, data.results[0].geometry.lng);
-            L.marker(latLng).addTo(map);
+            L.marker(latLng, { icon: myIcon }).addTo(map);  // Use the custom icon when adding a marker
         });
 }
 
